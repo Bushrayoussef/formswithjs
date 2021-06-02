1b4f4e81d72b6b7aedd25b41ab86acbcac5735d1
Bushrayoussef <76091532+Bushrayoussef@users.noreply.github.com>
2021-06-02 21:25:52 +0300

    myFirstform

---
+++ script.js
@@ -0,0 +1,10 @@
+async function register(){
+
+    const fullname = document.forms["register_form"]["Fullname"].value;
+    if (Fullname == ' '){
+        alert('Full name must be filled');
+        return false;
+    }
+
+
+}
